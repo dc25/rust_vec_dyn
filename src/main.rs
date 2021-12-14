7595d9a4e5a6968b3da0aaa3878b90f15733c1b8
Dave Compton <davecompton7@gmail.com>
2021-12-14 06:07:15 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+/*
+Demonstrates that you can work with a Vec of dyn traits even without a Box to hold them.
+Motivated by this response: https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=c11f8f2b3e5481faafb6367226de8c1e
+to this blog post: https://dev.to/magnusstrale/rust-trait-objects-in-a-vector-non-trivial-4co5
+*/
+
+
+#[derive(Debug, Copy, Clone)]
+struct Matrix {
+	foo: i32,
+}
+
+#[derive(Debug)]
+struct Material {
+	bar: i32,
+}
+
+trait Shape {
+    fn material(&self) -> &Material;
+    fn transformation(&self) -> Matrix;
+}
+
+// Original code
+#[derive(Debug)]
+struct Sphere {
+    transformation: Matrix, // FYI - implements Copy trait
+    material: Material
+}
+
+impl Shape for Sphere {
+    fn material(&self) -> &Material {
+        &self.material
+    }
+    fn transformation(&self) -> Matrix {
+        self.transformation
+    }
+}
+
+fn do_stuff(objects: Vec<&dyn Shape>) {
+    for i in objects {
+        println!("{:#?}", i.material());
+    }
+}
+
+fn main() {
+    let material = Material{bar: 1};
+    let transformation = Matrix{foo: 2};
+    let sphere = Sphere{transformation, material};
+    let shape = &sphere as &dyn Shape;
+    let v = vec![shape];
+    do_stuff(v);
+}
